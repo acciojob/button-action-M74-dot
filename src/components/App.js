7b09bbb185c45c5b6fe02ff4b2024d5395b538a1
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-09-25 11:56:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const [showBtn, setShowBtn] = useState(false);
   return (
     <div className="App" id="main">
       // Do not alter the main div
-      <p id="para" className={(showBtn? "hide" : "show")}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <p id="para" className={(showBtn? "show" : "hide")}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
       <button id="click" onClick={handleShow}>{(showBtn? "Hide" : "Show")}</button>
     </div>
   );
